30f595f8db93a0939f71a1a26d2fcd8e490f4d7d
Sagar Moriya <sagar.moriya@digiwhiz.com.au>
2023-09-30 13:48:40 +0530

    added validation to signup form

--- frontend/src/components/SignUpForm.jsx
+++ frontend/src/components/SignUpForm.jsx
@@ -9,6 +9,12 @@ const SignUpForm = () => {
         password:''
     })
 
+    const [errors, setErrors] = useState({
+        name: '',
+        email: '',
+        password:''
+      });
+
     const handeInputChange=(e)=>{
         const {name,value} = e.target;
         setUserData({
@@ -17,31 +23,76 @@ const SignUpForm = () => {
         })
     }
 
+    const validateForm = () => {
+        let valid = true;
+        const newErrors = { ...errors };
+    
+        // Name validation
+        if (userData.name.trim() === '') {
+          newErrors.name = 'Name is required';
+          valid = false;
+        } else {
+          newErrors.name = '';
+        }
+    
+        // Email validation
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(userData.email.trim()=== ''){
+            newErrors.email = 'Email is required';
+            valid=false;
+        }
+         else if (!emailRegex.test(userData.email.trim())) {
+          newErrors.email = 'Invalid email address';
+          valid = false;
+        } else {
+          newErrors.email = '';
+        }
+
+         // Name validation
+         if (userData.password.trim() === '') {
+            newErrors.password = 'Password is required';
+            valid = false;
+          } else {
+            newErrors.password = '';
+          }
+    
+        setErrors(newErrors);
+        return valid;
+      };
+
     const handleSubmit=(e)=>{
         e.preventDefault();
-        console.log(userData);
+        if(validateForm()){
+
+            console.log(userData);
+        }
     }
+
+    
     return (
         <div className='singup-form'>
             <div className="content-wrapper">
             
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} noValidate>
             <div className="mb-3 group-input">
                     <label htmlFor="name" className="form-label subheading">Name</label>
                     <input type="text" className="form-control" id="name" name="name" aria-describedby="nameHelp" value={userData.name} onChange={handeInputChange}/>
-                    
-                
                 </div>
+                    {errors.name && <p className='text-danger'>{errors.name}</p>}
+
+
                 <div className="mb-3 group-input">
                     <label htmlFor="email" className="form-label subheading">Email address</label>
-                    <input type="email" className="form-control" id="email" name='email' aria-describedby="emailHelp" value={userData.email} onChange={handeInputChange}/>
-                    
-                
+                    <input type="email" className="form-control" id="email" name='email' aria-describedby="emailHelp" value={userData.email} onChange={handeInputChange} required='false'/>
                 </div>
+                {errors.email && <p className='text-danger'>{errors.email}</p>}
+
+
                 <div className="mb-3 group-input">
                     <label htmlFor="password" className="form-label subheading">Password</label>
                     <input type="password" className="form-control" id="password" name='password' value={userData.password} onChange={handeInputChange}/>
                 </div>
+                    {errors.password && <p className='text-danger'>{errors.password}</p>}
                 
                 <button type="submit" className="btn btn-primary mb-4">Create Account</button>
                 <p>Already a Member ? <span style={{color:"#27c9d8"}} onClick={()=>navigate("/user/login")}>Log In</span></p>
