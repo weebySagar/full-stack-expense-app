58f5a665f808c81212bbcec42d376a547b73090b
Sagar Moriya <sagar.moriya@digiwhiz.com.au>
2023-09-30 16:06:46 +0530

    Make the backend api of the login

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,3 +1,4 @@
+const { where } = require('sequelize');
 const User = require('../models/user-model');
 
 exports.addUser=async(req,res)=>{
@@ -7,5 +8,27 @@ exports.addUser=async(req,res)=>{
        await res.status(201).json(response)
     } catch (error) {
         res.status(400).json({error:error.errors[0].message})
+    }
+}
+
+exports.loginUser = async(req,res)=>{
+    const {email,password} = req.body;
+    try {
+        const user = await User.findOne({where:{email}});
+        
+        if(user){
+            const isPasswordValid =  user.dataValues.password == password;
+            if(isPasswordValid){
+                res.status(200).json({message:'User login successfully'})
+            }
+            else{
+                res.status(401).json({error:'User not authorized'})
+            }
+        }
+        else{
+            res.status(404).json({error:'User not found'})
+        }
+    } catch (error) {
+        res.status(500).json({error:'Internal server error'})
     }
 }
