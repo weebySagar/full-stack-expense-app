f72022c112ea6abc1d01032485b1e1f3115a3ba9
Sagar Moriya <sagarmaurya814@gmail.com>
2023-12-16 21:18:34 +0530

    added premium  button on profile page

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import Profile from '../components/Profile'
-import { useAuth } from '../context/AuthContext';
+import React from "react";
+import Profile from "../components/Profile";
+import { useAuth } from "../context/AuthContext";
+import PremiumUser from "../components/PremiumUser";
+import { createPaymentOrder } from "../services/payment-api";
 
 const ProfilePage = () => {
-  const {user} = useAuth();
+  const { user } = useAuth();
 
   return (
-    <section className='profile-page vh-100 d-flex flex-column justify-content-center align-items-center'>
-            <div className="container">
-                {/* <div className="profile-wrapper">
-                    <div className="profile-container bg-7">
+    <section className="profile-page vh-100 d-flex flex-column justify-content-center align-items-center">
+      <div className="container">
+        <div className="profile-container">
+          <Profile user={user} />
+          {user.premiumUser ? (
+            <button disabled className=" premium-btn btn-primary">Premium User</button>
+          ) : (
+            <div className="d-flex justify-content-center">
 
-                    </div>
-                </div> */}
-                <Profile user={user}/>
+            <button onClick={() => createPaymentOrder()} className="buy-premium-btn mt-4 btn-primary">Buy Premium</button>
             </div>
+          )}
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
