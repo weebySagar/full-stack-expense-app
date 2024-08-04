cb4331450a4f3b4bf605b9f1df57615bb9d1e3eb
Sagar Moriya <sagarmaurya814@gmail.com>
2024-08-04 19:51:09 +0530

    added loading indicator

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ const DownloadedFile = require('./models/downloaded-file-model');
 
 dotenv.config();
 app.use(cors({
-  origin:["https://trackwise-app.vercel.app","http://localhost:5173"],
+  origin:["https://trackwise-app.vercel.app","http://localhost:5173","http://192.168.2.110:5173"],
   methods:["GET","POST","PUT","DELETE"],
   // allowedHeaders: ['Content-Type', 'Authorization'],
   credentials:true
